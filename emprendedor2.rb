a3daf8dd0ab628a011bc2c26ad00d740f65b9194
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-04-03 19:44:35 -0300

    Se agrego impuesto aplicados a las utilidades 35% y solo aplica si son positivos

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -10,5 +10,7 @@ usuarios_gratis = usuarios_normal - usuarios_premiun
 
 gastos = ARGV[3].to_i
 
-utilidad = (precio_premium * usuarios_premiun + precio_normal * usuarios_normal + precio_gratis * usuarios_gratis) - gastos
-puts utilidad
+utilidad = (precio_premium * usuarios_premiun + precio_normal * usuarios_normal + precio_gratis * usuarios_gratis) - gastosutilidad_neta = u * 0.65 if u > 0
+
+utilidad_neta = u * 0.65 if u > 0
+puts utilidad_neta
